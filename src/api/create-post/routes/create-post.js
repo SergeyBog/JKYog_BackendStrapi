1790a1b20f8b0338198b4765f517042df8e546d3
gourav.chouhan@jaiinfoway <gourav.chouhan@jaiinfoway.com>
2023-06-19 06:01:16 +0000

    intial commit

---
+++ src/api/create-post/routes/create-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * create-post router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::create-post.create-post');
---
+++ src/api/jkyogproduct/services/jkyogproduct.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * jkyogproduct service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::jkyogproduct.jkyogproduct');
