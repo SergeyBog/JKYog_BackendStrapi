1d4d98f3440a9d1c2b6499230d0fa8f5c9cd5a63
Satya Prakash <satya.prakash@jaiinfoway.com>
2023-07-07 13:26:59 +0530

    change password for postgres for testing

--- config/database.js
+++ config/database.js
@@ -56,7 +56,8 @@ module.exports = ({ env }) => {
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'jkyogi'),
         user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'admin'),
+        // password: env('DATABASE_PASSWORD', 'admin'),
+        password: env('DATABASE_PASSWORD', 'postgres'),
         // ssl: env.bool('DATABASE_SSL', false) && {
         //   key: env('DATABASE_SSL_KEY', undefined),
         //   cert: env('DATABASE_SSL_CERT', undefined),
